69db681d43cd9e31aa95ed38fe591735e6ac6bd2
bensanta <BenSanta@protonmail.com>
2022-02-28 11:43:46 +0100

    Temp Converter update
    added the option to convert again without needing to re-run the program

--- Temperature Converter/src/TemperatureConverter.java
+++ Temperature Converter/src/TemperatureConverter.java
@@ -24,32 +24,49 @@ public class TemperatureConverter {
     public static double convertFtoC(double temp) {
         System.out.println(((temp - 32) / 1.8)); // Formula to convert Fahrenheit to Celsius
         return 0;
-        
+
     }
     public static void main(String args[]){
 
+        //Try again option
+        int tryAgainValue = 0;
+
         // Asks user the temperature
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter a temperature: ");
-        int temp = sc.nextInt();
-        
-        // Asks user measurement type (F or C)
-        Scanner measureType = new Scanner(System.in);
-        System.out.println("Are you using Fahrenheit or Celsius? (f/c) ");
-        String type = measureType.nextLine();
-        // If Fahrenheit use the FtoC conversion method
-        if (type.equals("f")){
-            convertFtoC(temp);
-        }
-        // If Celsius use the CtoF conversion method
-        else if (type.equals("c")){
+        while (tryAgainValue == 0){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Please enter a temperature: ");
+            int temp = sc.nextInt();
+            
+            // Asks user measurement type (F or C)
+            Scanner measureType = new Scanner(System.in);
+            System.out.println("Are you using Fahrenheit or Celsius? (f/c) ");
+            String type = measureType.nextLine();
+           // If Fahrenheit use the FtoC conversion method
+            if (type.equals("f")){
+               convertFtoC(temp);
+            }
+            // If Celsius use the CtoF conversion method
+            else if (type.equals("c")){
             convertCtoF(temp);
-        }
-        // If anything else other then "f" or "c" - gives
-        // instructions on how to use the program
-        else {
-            System.out.println("Please choose either 'f' or 'c'");
-        }
-        
+            }
+            // If anything else other then "f" or "c" - gives
+            // instructions on how to use the program
+            else {
+                System.out.println("Please choose either 'f' or 'c'");
+            }
+
+            //Prompts the user to convert again 
+            Scanner again = new Scanner(System.in);
+            System.out.println("Would you like to convert a different value? (y/n) ");
+            String tryAgain = again.nextLine();
+
+            if (tryAgain.equals("n")){
+                tryAgainValue = 1;  //If they don't want to convert another value, 
+                                    //the while loop's condition isn't met
+            }
+            else{
+                tryAgainValue = 0;
+            }
+            }
     }
 }
